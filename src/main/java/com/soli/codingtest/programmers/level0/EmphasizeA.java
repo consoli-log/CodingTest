6f5048a7b2789cdafea046023d3949f8d48b8192
consoli.log <eumsoli@gmail.com>
2025-05-28 10:28:30 +0900

    [프로그래머스] 181874 - A 강조하기 (문자열)

---
+++ src/main/java/com/soli/codingtest/programmers/level0/EmphasizeA.java
@@ -0,0 +1,34 @@
+package com.soli.codingtest.programmers.level0;
+
+/**
+ * 프로그래머스 - A 강조하기
+ * 난이도: 🤍 Level 0
+ * 유형: 문자열
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181874
+ */
+
+public class EmphasizeA {
+
+    public String solution(String myString) {
+        char[] str = myString.toCharArray();
+
+        for (int i = 0; i < myString.length(); i++) {
+            // a나 A인 경우는 대문자로
+            if (myString.charAt(i) == 'a' || myString.charAt(i) == 'A'){
+                str[i] = myString.toUpperCase().charAt(i);
+            } else {
+                str[i] = myString.toLowerCase().charAt(i);
+            }
+        }
+
+        String answer = new String(str);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        EmphasizeA ea = new EmphasizeA();
+        System.out.println(ea.solution("abstract algebra")); // AbstrAct AlgebrA
+        System.out.println(ea.solution("PrOgRaMmErS")); // progrAmmers
+    }
+}
