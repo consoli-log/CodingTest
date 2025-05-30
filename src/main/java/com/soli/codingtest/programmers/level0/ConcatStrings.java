2d7a872ad40f1aa71b21cdcd6344a47c61583f6d
consoli.log <eumsoli@gmail.com>
2025-05-30 14:32:44 +0900

    [프로그래머스] 181946 - 문자열 붙여서 출력하기 (문자열 / 입력 / 문자열 연결)

---
+++ src/main/java/com/soli/codingtest/programmers/level0/ConcatStrings.java
@@ -0,0 +1,22 @@
+package com.soli.codingtest.programmers.level0;
+
+/**
+ * 프로그래머스 - 문자열 붙여서 출력하기
+ * 난이도: 🤍 Level 0
+ * 유형: 문자열 / 입력 / 문자열 연결
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181946
+ */
+
+import java.util.Scanner;
+
+public class ConcatStrings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a + b);
+    }
+}
+
