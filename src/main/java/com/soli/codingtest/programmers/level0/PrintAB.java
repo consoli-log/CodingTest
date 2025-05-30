1b44240d8463498751aa4155b7546b14148cc4b7
consoli.log <eumsoli@gmail.com>
2025-05-30 14:07:06 +0900

    [프로그래머스] 181951 - a와 b 출력하기 (입출력 / 형식 출력 / Scanner)

---
+++ src/main/java/com/soli/codingtest/programmers/level0/PrintAB.java
@@ -0,0 +1,23 @@
+package com.soli.codingtest.programmers.level0;
+
+/**
+ * 프로그래머스 - a와 b 출력하기
+ * 난이도: 🤍 Level 0
+ * 유형: 입출력 / 형식 출력 / Scanner
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181951
+ */
+
+import java.util.Scanner;
+
+public class PrintAB {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);  // a = 4
+        System.out.println("b = " + b);  // b = 5
+    }
+}
+
