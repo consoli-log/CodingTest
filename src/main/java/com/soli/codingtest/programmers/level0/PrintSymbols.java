fbb63abaf37e0213a96f8a7c021856865909eaae
consoli.log <eumsoli@gmail.com>
2025-05-30 14:20:34 +0900

    [프로그래머스] 181948 - 특수문자 출력하기 (출력 / 이스케이프 문자 처리)

---
+++ src/main/java/com/soli/codingtest/programmers/level0/PrintSymbols.java
@@ -0,0 +1,16 @@
+package com.soli.codingtest.programmers.level0;
+
+/**
+ * 프로그래머스 - 특수문자 출력하기
+ * 난이도: 🤍 Level 0
+ * 유형: 출력 / 이스케이프 문자 처리
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181948
+ */
+
+public class PrintSymbols {
+
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
+
