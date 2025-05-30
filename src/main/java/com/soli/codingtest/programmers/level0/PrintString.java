3a9814ea78ab93fdda2f70b43aaf53c6118e19e4
consoli.log <eumsoli@gmail.com>
2025-05-30 13:59:10 +0900

    [프로그래머스] 181952 - 문자열 출력하기 (입출력 / Scanner / 기본 문법)

---
+++ src/main/java/com/soli/codingtest/programmers/level0/PrintString.java
@@ -0,0 +1,21 @@
+package com.soli.codingtest.programmers.level0;
+
+/**
+ * 프로그래머스 - 문자열 출력하기
+ * 난이도: 🤍 Level 0
+ * 유형: 입출력 / Scanner / 기본 문법
+ * 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181952
+ */
+
+import java.util.Scanner;
+
+public class PrintString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(str);  // abc
+    }
+}
+
